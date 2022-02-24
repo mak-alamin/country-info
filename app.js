726df7045e8b800b3ad9b8c519303f50407b1f06
Mak Alamin <makalamin012@gmail.com>
2022-02-24 22:37:41 +0600

    loaded details with alpha code

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// Load Countries
+function loadCountries() {
+  const countriesList = document.getElementById("countries");
+  countriesList.innerHTML = "Loading...";
+  fetch("https://restcountries.com/v2/all")
+    .then((response) => response.json())
+    .then((countries) => {
+      countriesList.innerHTML = "";
+
+      countries.forEach((country) => {
+        let singleCountry = `<div class="card">
+     <img src="${country.flag}" class="card-img-top" alt="${country.name}" />
+     <div class="card-body">
+       <h5 class="card-title">${country.name}</h5>
+       <p class="card-text">Capital: <strong> ${country.capital} </strong></p>
+       <a href="#" class="btn btn-primary" onclick="loadCountryDetails('${country.alpha2Code.toLowerCase()}')">View Details</a>
+     </div>
+   </div>`;
+        countriesList.innerHTML += singleCountry;
+      });
+    });
+}
+
 // Search country
 function searchCountry(searchTerm) {
   let countries = document.getElementsByClassName("card-title");
@@ -10,59 +33,32 @@ function searchCountry(searchTerm) {
   }
 }
 
-function loadCountries(count) {
-  const countriesList = document.getElementById("countries");
-  countriesList.innerHTML = "Loading...";
-  fetch(`https://restcountries.com/v2/${count}`)
-    .then((response) => response.json())
-    .then((countries) => {
-      countriesList.innerHTML = "";
-      countries.forEach((country) => {
-        let singleCountry = `<div class="card">
-     <img src="${country.flag}" class="card-img-top" alt="${country.name}" />
-     <div class="card-body">
-       <h5 class="card-title">${country.name}</h5>
-       <p class="card-text">Capital: <strong> ${country.capital} </strong></p>
-       <a href="#" class="btn btn-primary" onclick="loadCountryDetails('${country.name.toLowerCase()}')">View Details</a>
-     </div>
-   </div>`;
-        countriesList.innerHTML += singleCountry;
-      });
-    });
-}
-
-function loadCountryDetails(name) {
-  let countryName = name.replace("Å", "A").split(" ")[0];
-
-  fetch(`https://restcountries.com/v2/name/${countryName}`)
+// Load Country Details
+function loadCountryDetails(code) {
+  fetch(`https://restcountries.com/v2/alpha/${code}`)
     .then((response) => response.json())
     .then((country) => {
+      console.log(country);
       const countryDetails = document.getElementById("country-details");
-
-      let index = 0;
-
-      if (countryName == "india") {
-        index = 1;
-      }
 
       countryDetails.innerHTML = `
       <div class="card">
-      <img src="${country[index].flag}" class="card-img-top" alt="${country[index].name}" />
+      <img src="${country.flag}" class="card-img-top" alt="${country.name}" />
       <div class="card-body">
-        <h5 class="card-title">${country[index].name}</h5>
-        <p class="card-text">Capital: <strong> ${country[index].capital} </strong></p>
-        <p class="card-text">Population: <strong> ${country[index].population} </strong></p>
-        <p class="card-text">Region: <strong> ${country[index].region} </strong></p>
-        <p class="card-text">Subregion: <strong> ${country[index].subregion} </strong></p>
-        <p class="card-text">Timezone: <strong> ${country[index].timezones} </strong></p>
-        <p class="card-text">Currency: <strong> ${country[index].currencies} </strong></p>
-        <p class="card-text">Languages: <strong> ${country[index].languages} </strong></p>
-        <p class="card-text">Borders: <strong> ${country[index].borders} </strong></p>
+        <h5 class="card-title">${country.name}</h5>
+        <p class="card-text">Capital: <strong> ${country.capital} </strong></p>
+        <p class="card-text">Population: <strong> ${country.population} </strong></p>
+        <p class="card-text">Region: <strong> ${country.region} </strong></p>
+        <p class="card-text">Subregion: <strong> ${country.subregion} </strong></p>
+        <p class="card-text">Timezone: <strong> ${country.timezones} </strong></p>
+        <p class="card-text">Currency: <strong> ${country.currencies[0].name} (${country.currencies[0].symbol}) </strong></p>
+        <p class="card-text">Languages: <strong> ${country.languages} </strong></p>
+        <p class="card-text">Borders: <strong> ${country.borders} </strong></p>
       </div>
     </div>
       `;
     });
 }
 
-document.onload = loadCountryDetails("bangladesh");
-document.onload = loadCountries("all");
+document.onload = loadCountries();
+document.onload = loadCountryDetails("af");
